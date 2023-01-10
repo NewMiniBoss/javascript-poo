5150b2e554a737a8d65359f71eb94b824055c3f3
NewMiniBoss <gabriel.f.lazari@gmail.com>
2023-01-10 19:42:44 -0300

    POO

---
+++ index.js
@@ -0,0 +1,7 @@
+// let dataComConstrutor = new Date()
+// let data1 = {
+//     'dia': dataComConstrutor.getDate(),
+//     'mes': dataComConstrutor.getMonth(),
+//     'ano': dataComConstrutor.getFullYear()
+// }
+// console.log(data1) 
