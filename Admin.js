1ee8667f01da7a6db7e0b7c2091f57da24dbcc07
NewMiniBoss <gabriel.f.lazari@gmail.com>
2023-01-17 10:27:49 -0300

    class / package.json / admin

---
+++ Admin.js
@@ -0,0 +1,5 @@
+import User from "./User.js";
+
+const gabrielUser = new User(`gabriel fernandes lazari`, `gabriel76_@gmail.com`, `11/01/2000`);
+console.log(gabrielUser);
+console.log(gabrielUser.exibirInfo());
