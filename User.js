d3315601dc0c72e27f7981270c2731a929d18d71
NewMiniBoss <gabriel.f.lazari@gmail.com>
2023-01-18 13:46:58 -0300

    metodo privado

--- User.js
+++ User.js
@@ -1,13 +1,35 @@
 export default class User {
+    #nome
+    #email
+    #nascimento
+    #role
+    #ativo
     constructor(nome, email, nascimento, role, ativo = true) {
-        this.nome = nome;
-        this.email = email;
-        this.nascimento = nascimento;
-        this.role = role || 'estudante';
-        this.ativo = ativo;
+        this.#nome = nome;
+        this.#email = email;
+        this.#nascimento = nascimento;
+        this.#role = role || 'estudante';
+        this.#ativo = ativo;
     }
 
-    exibirInfo() {
-        return `${this.nome}, ${this.email}, ${this.nascimento}`;
+    // #montaObjUser() {
+    //     return ({
+    //         nome: this.#nome,
+    //         email: this.#email,
+    //         nascimento: this.#nascimento,
+    //         role: this.#role,
+    //         ativo: this.#ativo
+    //     })
+    // }
+
+    get exibirInfo() {
+        return [this.#nome, this.#email, this.#nascimento, this.#role, this.#ativo]
+
+
     }
+
+    // exibirInfo() {
+    //     const objUser = this.#montaObjUser();
+    //     return `${objUser.nome}, ${objUser.email}, ${objUser.nascimento}, ${objUser.role}, ${objUser.ativo}`;
+    // }
 }
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import User from "./User.js";
 import Admin from "./Admin.js";
 import Docente from "./Docente.js";
+
+const novoUser = new User(`gabriel`, `gabs@gmail.com`, `11/01/2000`);
+// console.log(novoUser.exibirInfo());
+
+const novoAdmin = new Admin(`joao`, `jl@gmail.com`, `01/06/1969`);
+console.log(novoAdmin.exibirInfo)
